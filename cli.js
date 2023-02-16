71ca2df3a2b2ab9b5947aacdac1fc8cb9d72356c
Ana Cecilia Toribio Arrieta <cecitoribio@MacBook-Pro-de-Ana.local>
2023-02-16 15:16:48 -0500

    funciona los comendos de linas segun las opciones dadas

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
-console.log('hello wordl')
+//console.log('hello wordl')
 
+//const path = require('path');
 const process = require('process');
 //const { argv } = require('process')
 const { mdLinks } = require('./index.js');
@@ -9,17 +10,29 @@ const { mdLinks } = require('./index.js');
 // array con path del bin y del archivo actual
 //console.log(argv)
 const arg1 = process.argv
-console.log(arg1)
+//console.log(arg1)
 const arg2 = process.argv[1]
-console.log(arg2)
+//console.log(arg2)
 const route = process.argv[2]
-//console.log(arg3)
+console.log(route)
 const opcion1 = process.argv[3]
 //console.log(arg4)
-const opcion2 = process.argv[3]
+const opcion2 = process.argv[4]
 
 // const [, , ...args] = process.argv
-if (opcion1) {
-    mdLinks(route)
-        .then(resul => console.log(resul))
+
+if (route) {
+    if (opcion1 === undefined && opcion2 === undefined) {
+        mdLinks(route, { validate: false, stats: false })
+            .then(result => result)
+    } else if (opcion1 === '--validate' && opcion2 === undefined) {
+        mdLinks(route, { validate: true, stats: false })
+            .then(result => result)
+    } else if (opcion1 === '--stats' && opcion2 === undefined) {
+        mdLinks(route, { validate: false, stats: true })
+            .then(result => result)
+    } else if ((opcion1 === '--validate' && opcion2 === '--stats') || (opcion1 === '--stats' && opcion2 === '--validate')) {
+        mdLinks(route, { validate: true, stats: true })
+            .then(result => result)
+    } 
 }
